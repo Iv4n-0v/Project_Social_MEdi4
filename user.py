76727a4864bf98d034e7867041bc23393dcd279a
Ivan <vanegasivanc@gmail.com>
2025-12-03 16:12:42 -0500

    SessionDep

--- user.py
+++ user.py
@@ -10,7 +10,7 @@ from supa.supabase import upload_to_bucket
 router = APIRouter(tags=["users"])
 
 @router.get("", response_class=HTMLResponse)
-def get_all_users(request: Request, session: Session = Depends(SessionDep)):
+def get_all_users(request: Request, session: SessionDep):
     users = session.exec(select(User)).all()
     return request.app.state.templates.TemplateResponse(
         "user_list.html",
@@ -30,7 +30,7 @@ def show_create(request: Request):
 @router.post("/", response_model=User)
 async def create_user(
     request: Request,
-    session: Session=Depends(SessionDep),
+    session: SessionDep,
     name: str = Form(...),
     type: str = Form(...),
     is_active: bool = Form(True),
