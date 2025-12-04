80e6b29ee88bc0d4bc88592e739d28a447bf9ec8
Ivan <vanegasivanc@gmail.com>
2025-12-03 20:30:21 -0500

    change again in the supabase

--- supa/supabase.py
+++ supa/supabase.py
@@ -8,27 +8,27 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-async def upload_to_bucket(file: UploadFile, folder: str = "users", bucket: str = "bucket"):
-    try:
-        content = await file.read()
-        extension = os.path.splitext(file.filename)[1].lower()
-        unique_name = f"{uuid.uuid4()}{extension}"
-        file_path = f"{folder}/{unique_name}"
-        response = supabase.storage.from_(bucket).upload(
-            file_path,
-            content,
-            {
-                "content-type": file.content_type,
-                "cache-control": "3600",
-                "upsert": False
-            }
-        )
-        if hasattr(response, "error") and response.error is not None:
-            raise Exception(response.error.message)
-        public_url = f"{SUPABASE_URL}/storage/v1/object/public/{bucket}/{file_path}"
+async def upload_to_bucket(file: UploadFile, folder: str = "users"):
 
-        return public_url
+    bucket = "bucket"
+    content = await file.read()
+    extension = os.path.splitext(file.filename)[1].lower()
 
-    except Exception as e:
-        print("Upload error:", str(e))
-        return None
+    unique_name = f"{uuid.uuid4()}{extension}"
+
+    file_path = f"{folder}/{unique_name}"
+
+    supabase.storage.from_(bucket).upload(
+        file_path,
+        content,
+        {
+            "content-type": file.content_type,
+            "cache-control": "no-cache",
+            "upsert": False
+        }
+    )
+
+    public_url = f"{SUPABASE_URL}/storage/v1/object/public/{bucket}/{file_path}"
+    public_url = f"{public_url}?v={uuid.uuid4()}"
+
+    return public_url
