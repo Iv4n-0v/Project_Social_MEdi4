2d44a22a14d5528ab8880af4005448fd745846ad
Juan419 <tu-correo@gmail.com>
2026-09-16 17:54:31 -0500

    se crea el router con sus endpoints basicos

---
+++ backend/microservicios/beneficios/router.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, HTTPException
+from sqlmodel import select
+from microservicios.servicio_beneficios.database import SessionDep
+from microservicios.servicio_beneficios.models import Benefit, BenefitBase
+
+router = APIRouter(tags=["benefits"])
+
+@router.post("/", response_model=Benefit)
+def create_benefit(new_benefit: BenefitBase, session: SessionDep):
+    benefit = Benefit.model_validate(new_benefit)
+    session.add(benefit)
+    session.commit()
+    session.refresh(benefit)
+    return benefit
+
+@router.get("/", response_model=list[Benefit])
+def get_all_benefits(session: SessionDep):
+    return session.exec(select(Benefit)).all()
+
+@router.get("/{benefit_id}", response_model=Benefit)
+def get_benefit(benefit_id: int, session: SessionDep):
+    benefit = session.get(Benefit, benefit_id)
+    if not benefit:
+        raise HTTPException(status_code=404, detail="Benefit not found")
+    return benefit
