080a34de65aacd5bceb8edc76fa1902ab946f093
Iv4n-0v <vanegasivanc@gmail.com>
2026-09-23 23:00:07 +0000

    Pruebas del Delete, Crud de beneficios

--- backend/microservicios/beneficios/router.py
+++ backend/microservicios/beneficios/router.py
@@ -36,3 +36,11 @@ def update_benefit(benefit_id: int, benefit_data: BenefitBase, session: SessionD
     session.refresh(benefit)
     return benefit
 
+@router.delete("/{benefit_id}")
+def delete_benefit(benefit_id: int, session: SessionDep):
+    benefit = session.get(Benefit, benefit_id)
+    if not benefit:
+        raise HTTPException(status_code=404, detail="Benefit not found")
+    session.delete(benefit)
+    session.commit()
+    return {"message": "Benefit deleted successfully"}
